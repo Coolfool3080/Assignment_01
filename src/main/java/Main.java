daa3f5bb31ff229ead1b5dcad1b1418ccabbe130
Coolfool3080 <139318309+Coolfool3080@users.noreply.github.com>
2023-10-19 20:41:05 -0700

    Added reverseNumber method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         NumberManipulator calc = new NumberManipulator();
 
-        System.out.println(calc.sumOfDigits(1000));
+        System.out.println(calc.sumOfDigits(456));
+        System.out.println(calc.reverseNumber(679));
     }
 }
--- src/main/java/NumberManipulator.java
+++ src/main/java/NumberManipulator.java
@@ -2,7 +2,7 @@ public class NumberManipulator {
     static final int BASE_10_NUMERAL= 10;
 
     public int sumOfDigits(int number_input) {
-        //validate that the passed number_1 is a valid 3-digit number.
+        //validate that the passed number_input is a valid 3-digit number.
         if(number_input<100 || number_input >999)
         {
             //return -1 if invalid for troubleshooting
@@ -29,7 +29,29 @@ public class NumberManipulator {
     }
 
     public String reverseNumber(int number_input) {
+        //validate that the passed number_input is a valid 3-digit number.
+        if(number_input<100 || number_input >999)
+        {
+            //return -1 if invalid for troubleshooting
+            return "Error: Invalid 3 digit number.";
+        }
 
-        return null;
+        int third_digit;
+        third_digit = number_input%10;
+        number_input -= third_digit;
+        number_input /= BASE_10_NUMERAL;
+
+        int second_digit;
+        second_digit = number_input%10;
+        number_input -= second_digit;
+        number_input /= BASE_10_NUMERAL;
+
+        int first_digit;
+        first_digit = number_input%10;
+
+        String reverse_number = null;
+        reverse_number = String.format("%d%d%d", third_digit, second_digit, first_digit);
+
+        return reverse_number;
     }
 }
