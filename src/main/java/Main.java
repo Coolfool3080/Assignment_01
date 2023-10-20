963ddc7496816020b5ea0ad0be9be2f01ebd9b35
Coolfool3080 <139318309+Coolfool3080@users.noreply.github.com>
2023-10-19 21:39:56 -0700

    Clean up main according to Assignment directions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Author: Aaron M. Arreola
  * File  : Main.java
@@ -5,14 +7,28 @@
  */
 public class Main {
     public static void main(String[] args) {
-        NumberManipulator calc = new NumberManipulator();
+        // create scanner for input from the keyboard.
+        Scanner keyboard = new Scanner(System.in);
 
-        System.out.println(calc.sumOfDigits(456));
-        System.out.println(calc.reverseNumber(679));
+        //receive user input for a 3-digit number
+        System.out.print("Enter a 3-digit number: ");
+        int user_3_digit_number = keyboard.nextInt();
 
-        int[] reversed_number = calc.toArray(258);
+        //make instance of NumberManipulator object
+        NumberManipulator manipulator = new NumberManipulator();
+
+        //call sumOfDigits
+        int sum_of_digits = manipulator.sumOfDigits(user_3_digit_number);
+        System.out.println(sum_of_digits);
+
+        //call reverseNumber
+        String reversed_numbers = manipulator.reverseNumber(user_3_digit_number);
+        System.out.println(reversed_numbers);
+
+        //call toArray
+        int[] array_of_digits = manipulator.toArray(user_3_digit_number);
         for (int i = 0; i < 3; ++i) {
-            System.out.println(reversed_number[i]);
+            System.out.println(array_of_digits[i]);
         }
     }
 }
